41c45e5cffe67fb825bf6dd1ba14851119e4b326
Juan Pablo <juanpablo10056@gmail.com>
2026-05-03 00:29:21 -0500

    Primer commit: Se inicializo proyecto en spring boot

---
+++ src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.aurora.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AuroraApplication.class, args);
+	}
+
+}
