0ea76fa081181a05b42c155ca4d5d87c1c6877d5
Kila005 <maria.diaz7502@uco.net.co>
2026-05-15 18:56:23 -0500

    ajuste en AuroraAplication, antes te pedia un usuario y contraseña q salian x aca ahora activamos spring security q estaba desactivado, pq no teniamos una dependencia de auth 0 para manejar todo ese tema gracias a scanBasePackages va a leer mi archivo de seguridad

--- src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
+++ src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
@@ -2,14 +2,8 @@ package co.edu.uco.aurora.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-
-@SpringBootApplication(exclude = {
-		SecurityAutoConfiguration.class,
-		UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication(scanBasePackages = "co.edu.uco.aurora")
 public class AuroraApplication {
 
 	public static void main(String[] args) {
