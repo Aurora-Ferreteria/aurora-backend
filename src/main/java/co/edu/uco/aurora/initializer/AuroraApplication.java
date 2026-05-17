d915c40ba63850d2879d8a56c4000865b386455e
Kila005 <maria.diaz7502@uco.net.co>
2026-05-17 09:30:39 -0500

    ci

--- src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
+++ src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
@@ -2,10 +2,16 @@ package co.edu.uco.aurora.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // <-- Agregar import
 
-@SpringBootApplication // Quitamos los excludes para que Spring Security funcione con todo su poder nativo
-@ComponentScan(basePackages = "co.edu.uco.aurora") // Obligamos a Spring a escanear TODOS los paquetes del proyecto
+@SpringBootApplication
+@ComponentScan(basePackages = "co.edu.uco.aurora")
+// 1. Obliga a Spring a escanear tus interfaces JpaRepository en esa subcarpeta
+@EnableJpaRepositories(basePackages = "co.edu.uco.aurora.infrastructure.persistence.repository.sql.jpa")
+// 2. Obliga a Spring a mapear las tablas/entidades que están dentro de la subcarpeta entity
+@EntityScan(basePackages = "co.edu.uco.aurora.infrastructure.persistence.repository.sql.jpa.entity")
 public class AuroraApplication {
 
 	public static void main(String[] args) {
