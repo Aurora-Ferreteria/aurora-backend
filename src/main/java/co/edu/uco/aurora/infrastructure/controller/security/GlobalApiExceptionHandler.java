6c6b06e85ad3747b7fd2426f2a7405495c10469a
Kila005 <maria.diaz7502@uco.net.co>
2026-05-23 13:19:30 -0500

    segundo intento

--- src/main/java/co/edu/uco/aurora/infrastructure/controller/security/GlobalApiExceptionHandler.java
+++ src/main/java/co/edu/uco/aurora/infrastructure/controller/security/GlobalApiExceptionHandler.java
@@ -1,32 +1,52 @@
 package co.edu.uco.aurora.infrastructure.controller.security;
 
+import co.edu.uco.aurora.crosscutting.exception.AuroraException;
+import co.edu.uco.aurora.crosscutting.messagescatalog.MessageCatalogService;
 import co.edu.uco.aurora.crosscutting.messagescatalog.MessagesEnum;
 import co.edu.uco.aurora.infrastructure.controller.dto.Response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public final class GlobalApiExceptionHandler {
+
+    private final MessageCatalogService messageCatalogService;
+
+    public GlobalApiExceptionHandler(MessageCatalogService messageCatalogService) {
+        this.messageCatalogService = messageCatalogService;
+    }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Response<String>> handleHttpMessageNotReadableException(
             HttpMessageNotReadableException ex, WebRequest request) {
 
         Response<String> responseObjectData = Response.createFailedResponse();
-        HttpStatus responseStatusCode = HttpStatus.BAD_REQUEST;
-
-        // Se asigna el mensaje genérico para cualquier error de formato o lectura
-        String userMessage = MessagesEnum.ERROR_HTTP_MESSAGE_NOT_READABLE_GENERIC.name();
+        String userMessage = messageCatalogService.getMessageContent(MessagesEnum.ERROR_HTTP_MESSAGE_NOT_READABLE_GENERIC);
 
         responseObjectData.addMessage(userMessage);
+        return new ResponseEntity<>(responseObjectData, HttpStatus.BAD_REQUEST);
+    }
 
-        ex.printStackTrace();
+    @ExceptionHandler(AuroraException.class)
+    public ResponseEntity<Response<String>> handleAuroraException(
+            AuroraException ex, WebRequest request) {
 
-        return new ResponseEntity<>(responseObjectData, responseStatusCode);
+        Response<String> responseObjectData = Response.createFailedResponse();
+        String translatedMessage;
+
+        try {
+            MessagesEnum enumKey = MessagesEnum.valueOf(ex.getUserMessage());
+            translatedMessage = messageCatalogService.getMessageContent(enumKey);
+        } catch (IllegalArgumentException e) {
+            translatedMessage = ex.getUserMessage();
+        }
+
+        responseObjectData.addMessage(translatedMessage);
+        return new ResponseEntity<>(responseObjectData, HttpStatus.BAD_REQUEST);
     }
 }
