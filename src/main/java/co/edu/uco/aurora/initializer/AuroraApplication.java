7590168a829c04965f0861d15ca0af7f7840b77b
Kila005 <maria.diaz7502@uco.net.co>
2026-05-23 18:37:29 -0500

    internacionalizacion fin

--- src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
+++ src/main/java/co/edu/uco/aurora/initializer/AuroraApplication.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-
-import java.util.Arrays;
-import java.util.Locale;
 
 @SpringBootApplication
 @EnableCaching
@@ -24,13 +18,5 @@ public class AuroraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuroraApplication.class, args);
-	}
-
-	@Bean
-	public LocaleResolver localeResolver() {
-		AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
-		resolver.setSupportedLocales(Arrays.asList(new Locale("es"), new Locale("en")));
-		resolver.setDefaultLocale(new Locale("es"));
-		return resolver;
 	}
 }
